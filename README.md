f446b72d2cee3268897bd47806a79aeb07020f7a
Arturo Cunillas Valadez <Arturo.valadez1@aol.com>
2023-04-20 22:20:49 -0700

    updted file structure and repurposed code

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # basic-auth
+
+## Phase 1 Requirements
+Today, we begin the first of a 3-Phase build of an authentication system, written in Express. The following core requirements detail the functionality for this phase of the project.
+
+As a user, I want to create a new account so that I may later login
+Using an HTTP REST client or a web form:
+Make a POST request to the/signup route with username and password.
+Your server should support both JSON and FORM data as input.
+On a successful account creation, return a 201 status with the user object in the body.
+On any error, trigger your error handler with an appropriate error.
+As a user, I want to login to my account so that I may access protected information
+Using am HTTP REST client, or a web form:
+Make a POST request to the /signin route.
+Send a basic authentication header with a properly encoded username and password combination.
+On a successful account login, return a 200 status with the user object in the body.
+On any error, trigger your error handler with the message “Invalid Login”.
+
+## lab 06 UML
+
+![Lab06UML](/lab6.png)
---
+++ src/auth/basic.test.js
@@ -0,0 +1,64 @@
+'use strict';
+
+// const supertest = require('supertest');
+// const server = require('../../src/server');
+// const { sequelize, userModel } = require('../../src/auth/models/model');
+const basic = require('./middleware/basic');
+
+const base64 = require('base-64');
+const bcrypt = require('bcrypt');
+// const request = supertest(server.app);
+
+// beforeAll(
+//   async () => {
+//     await sequelize.sync();
+//   }
+// )
+// afterAll(
+//   async () => {
+//     await sequelize.drop();
+//   }
+// )
+
+
+
+
+// test('Does the middleware function (send it a basic header)', async () => {
+//   const username = 'b';
+//   const password = 'b';
+//   const encodedCredentials = base64.encode(`${username}:${password}`);
+  
+//   const req = {
+//     headers: {
+//       authorization: `Basic ${encodedCredentials}`,
+//     }
+//   };
+
+//   const res = {
+//     status: jest.fn().mockReturnThis(),
+//     send: jest.fn()
+//   };
+  
+//   const nextF = jest.fn();
+//   await basic(req, res, nextF);
+//   expect(nextF).toHaveBeenCalled();
+// });
+
+
+// test('Does the middleware function (send it a basic header)', async () => {
+//   let login = {        
+//     username: "b",
+//   password: "b",
+// };
+//   const req = {
+//     headers: {
+//       authorization: `Basic ${login}`,
+//     }
+//   };
+
+//   const res = {};
+//   const nextF = jest.fn();
+//   await basic(req, res, nextF);
+//   expect(nextF).toHaveBeenCalled();
+// });
+
